fc6c9babdc087897cf2c7bfa55abe820b89af686
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-11 16:43:54 -0300

    Implemented Footer to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <Navigation />
   <router-view />
+  <Footer />
+
 </template>
 
 <script setup>
 import Navigation from './components/Navigation.vue'
+import Footer from './components/Footer.vue';
 
 </script>
 
