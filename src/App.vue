48dba8cdec09fb0972d22b4f679e5085589fb0c9
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-11 16:28:31 -0300

    added my css variables and deleted aboutview

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,15 @@
 <template>
   <Navigation />
   <router-view />
+  <Footer />
 </template>
 
 <script setup>
 import Navigation from './components/Navigation.vue'
+import Footer from './components/Footer.vue'
 
 </script>
 
 <style>
 @import '@/assets/base.css';
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 </style>
