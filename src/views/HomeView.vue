7240614644590aa3eab98fe663c621ef7a3ac096
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-14 14:24:21 -0300

    Added Projects.vue to the homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <Main />
   <About />
+  <Projects />
 </template>
 
 <script setup>
 import Main from '../components/Main.vue';
 import About from '../components/About.vue'
+import Projects from '../components/Projects.vue';
 </script>
