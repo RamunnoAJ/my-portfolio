222c184ea06d9a69ebc0a66c66bc65d91c522d9f
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-15 22:02:43 -0300

    new changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/',
+  base: '/ramunnoaj.github.io/',
   plugins: [vue()],
   resolve: {
     alias: {
